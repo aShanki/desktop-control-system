a0add8fa1257abe3d37baa410115e7d96d6613a1
aShanki <ashanksundaram2@gmail.com>
2026-03-17 11:47:54 +1100

    fix: remove unused demo assets and video project

--- demo-video/remotion.config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from "@remotion/cli/config";
-import { enableTailwind } from "@remotion/tailwind";
-
-Config.overrideWebpackConfig((config) => {
-  return enableTailwind(config);
-});
